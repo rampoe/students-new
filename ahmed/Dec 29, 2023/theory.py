73b73bc4d58d535c7375ed6fb5ef0c1a74a78344
Rahman Begjanov <mail.begman@gmail.com>
2023-12-29 20:07:12 +0500

    :speech_balloon: Small changes

--- ahmed/Dec 29, 2023/theory.py
+++ ahmed/Dec 29, 2023/theory.py
@@ -1,5 +1,22 @@
-a = 10
-b = 15
+# cars = ["BMW", "Opel", "Mercedes", "Audi", "Toyota"]
 
-if a + b == 25:
-    pass
+# for car in cars:
+#     print(car)
+
+# for i in range(len(cars)):
+#     print(cars[i])
+
+# counter = 0
+# while counter < len(cars):
+#     print(cars[counter])
+#     counter += 1
+
+# while cars:
+#     current_element = cars[0]
+#     print(current_element)
+#     cars.remove(current_element)
+# print(cars)
+
+# [print(car) for car in cars]
+
+# [print(car) for car in cars if "a" in car.lower()]
