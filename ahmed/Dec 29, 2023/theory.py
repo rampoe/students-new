03de0dc35b5de109918eb8da594c1068a1756756
Rahman Begjanov <mail.begman@gmail.com>
2023-12-29 20:13:31 +0500

    :art: Improve code

--- ahmed/Dec 29, 2023/theory.py
+++ ahmed/Dec 29, 2023/theory.py
@@ -1,22 +1,6 @@
-# cars = ["BMW", "Opel", "Mercedes", "Audi", "Toyota"]
+# names = ["Rahman", "Ahmed", "Ayhan", "Ali", "Rampoe"]
+# numbers = [100, 2, 50, 13]
 
-# for car in cars:
-#     print(car)
+# numbers.sort(reverse=True)
 
-# for i in range(len(cars)):
-#     print(cars[i])
-
-# counter = 0
-# while counter < len(cars):
-#     print(cars[counter])
-#     counter += 1
-
-# while cars:
-#     current_element = cars[0]
-#     print(current_element)
-#     cars.remove(current_element)
-# print(cars)
-
-# [print(car) for car in cars]
-
-# [print(car) for car in cars if "a" in car.lower()]
+# print(numbers)
