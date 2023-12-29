967f88494e21769d6b9a8aa53a89de268796da1d
Rahman Begjanov <mail.begman@gmail.com>
2023-12-29 20:53:40 +0500

    :art: Improve code

--- ahmed/Dec 29, 2023/theory.py
+++ ahmed/Dec 29, 2023/theory.py
@@ -1,5 +1,16 @@
-thistuple = ("apple", "banana", "cherry")
-y = ("orange",)
-thistuple += y
+# Not good
+# color1 = colors[0]
+# color2 = colors[1]
+# ....
 
-print(thistuple)
+color1, color2, color3, color4, color5, color6, color7 = (
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "light blue",
+    "dark blue",
+    "violet",
+)
+
+print(color3)
